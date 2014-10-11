0db0485d37a058ccf50ae55b4379140fd97ad090
Chris Horne <lahwran@lahwran.net>
2014-10-10 18:29:59 -0600

    Turn up default ca key size to 4096, because why not

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ echo ' *** Make sure to leave the challenge password empty! ***'
 echo
 
 mkdir -p incoming
-openssl req -nodes -new -newkey rsa:2048 -keyout ca.key.pem -out incoming/ca.csr.pem
+openssl req -nodes -new -newkey rsa:4096 -keyout ca.key.pem -out incoming/ca.csr.pem
 OPENSSL_CONF=ca.cnf openssl ca -selfsign -policy policy_ca -extensions ext_ca -in incoming/ca.csr.pem -out ca.crt.pem
 
 git add ca.crt.pem ca.key.pem certs/01.pem index.txt serial.txt
